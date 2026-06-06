e55cadf4f541dd9a29473ea752d0945cee926a06
Ny Hasina <djazejhasi@gmail.com>
2026-06-06 14:36:44 +0300

    update: need to correct the logic of the menu to make it more scalable and use class for a better maintenability

--- Scripts/script_main_menu.gd
+++ Scripts/script_main_menu.gd
@@ -1,7 +1,9 @@
 extends Node3D
 
-@onready var NormalMode: Button = $CanvasLayer/Container/MarginContainer/Gamemode/Normal;
-@onready var GameModeNode: VBoxContainer = $CanvasLayer/Container/MarginContainer/Gamemode;
+@onready var NormalMode: Button = $CanvasLayer/Container/MenuContainer/Gamemode/Normal;
+@onready var GameModeNode: VBoxContainer = $CanvasLayer/Container/MenuContainer/Gamemode;
+@onready var NormalModeNode: VBoxContainer = $CanvasLayer/Container/MenuContainer/NormalMode;
+@onready var MenuContainerNode: MarginContainer = $CanvasLayer/Container/MenuContainer;
 
 func	_on_button_hovered(button: Button):
 	button.grab_focus();
@@ -14,27 +16,39 @@ func	_on_button_pressed(button: Button):
 	create_tween().tween_property(button, "scale", Vector2(0.95, 0.95), 0.1);
 	match button.name:
 		"Normal":
-			print("Normal Pressed");
+			NormalModeNode.visible = true;
+			GameModeNode.visible = false;
+			NormalModeNode.get_child(0).grab_focus();
 		"Test":
 			pass;
 		"Options":
 			pass;
 		"Exit":
 			get_tree().quit(0);
+		"Level1":
+			print("Level1 button pressed.");
+		"Level2":
+			pass;
+		"Level3":
+			pass;
+		"Cancel":
+			NormalModeNode.visible = false;
+			GameModeNode.visible = true;
+			GameModeNode.get_child(0).grab_focus();
 
 func	_on_button_down(button: Button):
 	create_tween().tween_property(button, "scale", Vector2(0.95, 0.95), 0.1);
 
 func	_ready() -> void:
-	# Make this button grab focus for keyboard accessibility
-	NormalMode.grab_focus();
+	# Make the first button grab focus fssor keyboard accessibility
+	GameModeNode.get_child(0).grab_focus();
 	
-	for child in GameModeNode.get_children():
-		if child is Button:
-			child.mouse_entered.connect(_on_button_hovered.bind(child));
-			child.mouse_exited.connect(_on_button_blur.bind(child));
-			child.pressed.connect(_on_button_pressed.bind(child));
-			child.button_down.connect(_on_button_down.bind(child));
-			child.button_up.connect(_on_button_blur.bind(child));
-			child.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND;
-		pass;
+	for menuVerticalContainer in MenuContainerNode.get_children():
+		for child in menuVerticalContainer.get_children():
+			if child is Button:
+				child.mouse_entered.connect(_on_button_hovered.bind(child));
+				child.mouse_exited.connect(_on_button_blur.bind(child));
+				child.pressed.connect(_on_button_pressed.bind(child));
+				child.button_down.connect(_on_button_down.bind(child));
+				child.button_up.connect(_on_button_blur.bind(child));
+				child.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND;
